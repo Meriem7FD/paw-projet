041be7635862342c8ab0f39058c83bce7e851bd4
Meriem FOUDI <165810094+Meriem7FD@users.noreply.github.com>
2024-12-10 00:47:15 +0100

    Update Database.php

--- Database.php
+++ Database.php
@@ -1,23 +1,19 @@
 <?php
 class Database {
-    private $host = "localhost";
-    private $username = "root";
-    private $password = "meriemmimim30";
-    private $dbname = "minipaw";
+    private $host = 'localhost';
+    private $dbname = 'minipaw'; 
+    private $username = 'root'; 
+    private $password = 'meriemmimim30';
     private $conn;
 
-    public function __construct() {
-        // Créer une nouvelle connexion PDO
+    public function connect() {
         try {
-            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->dbname}", $this->username, $this->password);
+            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->dbname;charset=utf8mb4", $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $this->conn;
         } catch (PDOException $e) {
-            die("Erreur de connexion à la base de données : " . $e->getMessage());
+            die("Erreur de connexion : " . $e->getMessage());
         }
-    }
-
-    public function getConnection() {
-        return $this->conn;
     }
 }
 ?>
